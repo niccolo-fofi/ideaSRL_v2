35136d800353c337bdf3412f0100a882dc83e1db
niccolo-fofi <fofiniccolo@gmail.com>
2025-09-22 21:42:03 +0200

    no modifiche

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
     return(
-       <nav className="bg-amber-50 opacity-20 flex items-center"></nav>
+       <nav className="bg-amber-200 opacity-90 flex items-center"></nav>
     )
 }
 
